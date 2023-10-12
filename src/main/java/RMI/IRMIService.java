add84c2da43888abc7132a3170229393e7841e13
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-10-12 13:10:58 -0500

    uploadFile

--- src/main/java/RMI/IRMIService.java
+++ src/main/java/RMI/IRMIService.java
@@ -2,7 +2,7 @@ package RMI;
 import java.rmi.*;
 
 public interface IRMIService extends Remote {
-    public void uploadFile(byte[] mybyte, String path, int length) throws RemoteException;
+    public String uploadFile(byte[] fileData, String path, int length) throws RemoteException;
     public byte[] downloadFile(String servername) throws RemoteException;
     public String[] listFiles(String path) throws RemoteException;
     public boolean createDirectory(String path) throws RemoteException;
--- src/main/java/RMI/RMIService.java
+++ src/main/java/RMI/RMIService.java
@@ -1,4 +1,58 @@
 package RMI;
 
-public class RMIService {
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMIService extends UnicastRemoteObject implements IRMIService, Serializable {
+
+    protected RMIService(String path) throws RemoteException {
+        File storageDir = new File(path);
+        storageDir.mkdir();
+    }
+    @Override
+    public String uploadFile(byte[] fileData, String path, int length) throws RemoteException {
+        try {
+            File pathFile = new File(path);
+            FileOutputStream out = new FileOutputStream(pathFile);
+
+            out.write(fileData);
+            out.flush();
+            out.close();
+
+            return "Archivo subido exitosamente!";
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error al subir el archivo.";
+        }
+
+    }
+
+    @Override
+    public byte[] downloadFile(String servername) throws RemoteException {
+        return new byte[0];
+    }
+
+    @Override
+    public String[] listFiles(String path) throws RemoteException {
+        return new String[0];
+    }
+
+    @Override
+    public boolean createDirectory(String path) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean removeDirectoryOrFile(String path) throws RemoteException {
+        return false;
+    }
 }
