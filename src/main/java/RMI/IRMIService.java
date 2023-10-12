81f0ae40352cec9b0b93db8db045b0abcacee612
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-10-12 11:27:52 -0500

    basic server setup

--- src/main/java/RMI/IRMIService.java
+++ src/main/java/RMI/IRMIService.java
@@ -1,14 +1,11 @@
 package RMI;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.rmi.*;
-import java.rmi.registry.*;
 
 public interface IRMIService extends Remote {
-    public void uploadFile(byte[] mybyte, String serverpath, int length) throws RemoteException;
+    public void uploadFile(byte[] mybyte, String path, int length) throws RemoteException;
     public byte[] downloadFile(String servername) throws RemoteException;
-    public String[] listFiles(String serverpath) throws RemoteException;
-    public boolean createDirectory(String serverpath) throws RemoteException;
-    public boolean removeDirectoryOrFile(String serverpath) throws RemoteException;
+    public String[] listFiles(String path) throws RemoteException;
+    public boolean createDirectory(String path) throws RemoteException;
+    public boolean removeDirectoryOrFile(String path) throws RemoteException;
 }
 
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,5 +1,22 @@
-public class Server {
+import java.io.Serializable;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Server implements Serializable{
+
     public static void main(String[] args) {
 
+        try{
+
+            int portnumber = 1990;
+            Registry reg = LocateRegistry.createRegistry(portnumber);   //Creates and exports a Registry instance on the local host that accepts requests
+            //on the specified port.
+
+            System.out.println("Server is ready.");
+            System.out.println(portnumber);
+        }
+        catch(Exception e){
+            System.out.println("Server failed: " + e);
+        }
     }
 }
